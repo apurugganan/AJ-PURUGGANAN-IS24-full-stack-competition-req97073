8409b6afee9afa4595653dac9b9ab3b869092640
apurugganan <apurugganan1@my.bcit.ca>
2023-03-26 16:18:52 -0700

    add Custom Input component

---
+++ frontend/src/components/CustomInput.js
@@ -0,0 +1,14 @@
+
+function CustomInput(props){
+  return(
+    <div>
+       
+        <label>
+          {props.label}
+          <input type={props.type} value={props.value} onChange={props.onChange}/>
+        </label>
+      </div>
+  )
+}
+
+export default CustomInput;

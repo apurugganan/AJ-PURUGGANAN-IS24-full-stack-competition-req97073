c5e2a565cb5ccb72fe217c762b7732cc3429d49a
apurugganan <apurugganan1@my.bcit.ca>
2023-03-26 12:31:43 -0700

    added fetch and rendered data to landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,33 @@
+import { useState, useEffect} from 'react';
+
+// mockurl
+const url = process.env.URL || "http://localhost:3000/";
+
 
 function App() {
+  const [programs, setPrograms] = useState([]);
+
+  // use
+  useEffect( () => {
+    (async () => {
+      const response = await fetch(url);
+      const data = await response.json();    
+      setPrograms(data);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      hello from front endlanding
+      <h1>List of Programs</h1>
+      {programs.length > 0 ? (
+        <ul>
+          {programs.map((program) => (
+            <li key={program.productId}>{program.productName}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }

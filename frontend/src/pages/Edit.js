c65e21496c10011cd6de94eb380f63a6ee591a1b
apurugganan <apurugganan1@my.bcit.ca>
2023-03-27 10:59:31 -0700

    added useState and useEffect for Edit

--- frontend/src/pages/Edit.js
+++ frontend/src/pages/Edit.js
@@ -4,19 +4,19 @@ import ProgramForm from "../components/ProgramForm";
 
 function Edit({changeState}){
   const { productId } = useParams();
-  const [program, setProgram] = useState({})
+  const [program, setProgram] = useState({});
 
-  // useEffect( () => {
-  //   (async () => {
-  //     const response = await fetch(`http://localhost:8000/api/edit/${productId}`);
-  //     const data = await response.json();    
-    
-  //   })();
-  // }, []);
+  useEffect( () => {
+    (async () => {
+      const response = await fetch(`http://localhost:3000/api/edit/${productId}`);
+      const data = await response.json();    
+      setProgram(data)
+    })();
+  }, [productId]);
   return (
     <div>
       <h4>Edit Program {productId}</h4>
-        <ProgramForm changeState={changeState}/>
+        <ProgramForm changeState={changeState} program={program}/>
     </div>
   )
 }

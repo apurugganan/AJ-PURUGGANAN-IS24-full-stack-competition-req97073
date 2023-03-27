dd502d919f5238a42bf88696b515e6415ebd2f2e
apurugganan <apurugganan1@my.bcit.ca>
2023-03-26 20:06:55 -0700

    Added CustomInput Component

--- frontend/src/components/CustomInput.js
+++ frontend/src/components/CustomInput.js
@@ -2,12 +2,15 @@
 function CustomInput(props){
   return(
     <div>
-       
-        <label>
-          {props.label}
-          <input type={props.type} value={props.value} onChange={props.onChange}/>
-        </label>
-      </div>
+      <label>
+        {props.label}
+        <input 
+          type={props.type} 
+          value={props.value} 
+          onChange={props.onChange} 
+          />
+      </label>
+    </div>
   )
 }
 

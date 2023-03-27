ce662bf3c07ecfa08437ea8bfd72b042ce3c7a34
apurugganan <apurugganan1@my.bcit.ca>
2023-03-27 10:58:35 -0700

    modified ProgramFor for Edit feature

--- frontend/src/components/ProgramForm.js
+++ frontend/src/components/ProgramForm.js
@@ -1,44 +1,76 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import CustomInput from './CustomInput'
 
-function ProgramForm({changeState}){
+function ProgramForm({changeState, program, sendForm}){
+  const pName = program?.productName ?? "";
+  const pStart = program?.startDate?.split('/').join('-') ?? "";
+  const pMet = program?.methodology ?? "";
+  const pOwner = program?.productOwnerName ?? "";
+  const scrum = program?.scrumMasterName ?? "";
+  const dev1 = program?.developers?.[0] ?? "";
+  const dev2 = program?.developers?.[1] ?? "";
+  const dev3 = program?.developers?.[2] ?? "";
+  const dev4 = program?.developers?.[3] ?? "";
+  const dev5 = program?.developers?.[4] ?? "";
+
   const [productName, setProductName] = useState("");
-  const [productOwnerName, setProductOwnerName] = useState("");
-  const [scrumMasterName, setscrumMasterName] = useState("");
   const [startDate, setStartDate] = useState("");
   const [methodology, setMethodology] = useState("");
+  const [productOwnerName, setProductOwnerName] = useState("");
+  const [scrumMasterName, setscrumMasterName] = useState("");
   const [developer1, setDeveloper1] = useState("");
   const [developer2, setDeveloper2] = useState("");
   const [developer3, setDeveloper3] = useState("");
   const [developer4, setDeveloper4] = useState("");
   const [developer5, setDeveloper5] = useState("");
-  
-  const [error, setError] = useState("")
+
+  useEffect(()=>{
+    (() => {
+      setProductName(pName)
+      setStartDate(pStart)
+      setMethodology(pMet)
+      setProductOwnerName(pOwner)
+      setscrumMasterName(scrum)
+      setDeveloper1(dev1)
+      setDeveloper2(dev2)
+      setDeveloper3(dev3)
+      setDeveloper4(dev4)
+      setDeveloper5(dev5)
+    })();
+  },[
+    pName,
+    pStart,
+    pMet,
+    pOwner,
+    scrum,
+    dev1,
+    dev2,
+    dev3,
+    dev4,
+    dev5
+  ])
 
   async function handleSubmit(evt){
-    console.log("handle submit fired")
     evt.preventDefault()
-    if(!productName || !startDate || !productOwnerName || 
-      !scrumMasterName || !developer1 || !methodology){ 
+    if(!productName 
+      || !startDate 
+      || !methodology
+      || !productOwnerName 
+      || !scrumMasterName 
+      // || !developer1
+      ){ 
       console.log('incomplete required field')
       return
     }
 
+    
     const developers = [developer1];
+    if(developer2){developers.push(developer2)}
+    if(developer3){developers.push(developer3)}
+    if(developer4){developers.push(developer4)}
+    if(developer5){developers.push(developer5)}
 
-    if(developer2){
-      developers.push(developer2)
-    }
-    if(developer3){
-      developers.push(developer3)
-    }
-    if(developer4){
-      developers.push(developer4)
-    }
-    if(developer5){
-      developers.push(developer5)
-    }
-
+    // create object to be passed
     const body = {
       productName,
       startDate,
@@ -48,17 +80,10 @@ function ProgramForm({changeState}){
       developers
     }
 
-    const response = await fetch(`http://localhost:3000/api/programs`, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json'
-      }});
-    
-    const data = await response.json();
-    
-    changeState(data)
-      
+    // network call
+    const data = await sendForm(body);
+    // let react know changing state
+    changeState(data);
   }
 
   return (
@@ -67,12 +92,15 @@ function ProgramForm({changeState}){
         label="Program Name" 
         type="text" 
         value={productName} 
-        onChange={(e) => setProductName(e.target.value)}/>
+        onChange={(e) => setProductName(e.target.value)}
+      
+        />
       <CustomInput 
         label="Start Date" 
         type="date" 
         value={startDate} 
-        onChange={(e) => setStartDate(e.target.value)}/>
+        onChange={(e) => setStartDate(e.target.value)}
+        />
       
       <div>
         <label>
@@ -92,43 +120,49 @@ function ProgramForm({changeState}){
         label="Product Owner" 
         type="text" 
         value={productOwnerName} 
-        onChange={(e) => setProductOwnerName(e.target.value)}/>
-
+        onChange={(e) => setProductOwnerName(e.target.value)}
+        />
       <CustomInput 
         label="Scrum Master" 
         type="text" 
         value={scrumMasterName} 
-        onChange={(e) => setscrumMasterName(e.target.value)}/>
-
-
+        onChange={(e) => setscrumMasterName(e.target.value)}
+        />
       <CustomInput 
         label="Developer 1" 
         type="text" 
         value={developer1} 
-        onChange={(e) => setDeveloper1(e.target.value)}/>
+        onChange={(e) => setDeveloper1(e.target.value)}
+        />
       <CustomInput 
         label="Developer 2" 
         type="text" 
         value={developer2} 
-        onChange={(e) => setDeveloper2(e.target.value)}/>
+        onChange={(e) => setDeveloper2(e.target.value)} 
+        />
       <CustomInput 
         label="Developer 3" 
         type="text" 
         value={developer3} 
-        onChange={(e) => setDeveloper3(e.target.value)}/>
+        onChange={(e) => setDeveloper3(e.target.value)}
+        />
       <CustomInput 
         label="Developer 4" 
         type="text" 
         value={developer4} 
-        onChange={(e) => setDeveloper4(e.target.value)}/>
+        onChange={(e) => setDeveloper4(e.target.value)}
+        />
       <CustomInput 
         label="Developer 5" 
         type="text" 
         value={developer5} 
-        onChange={(e) => setDeveloper5(e.target.value)}/>
+        onChange={(e) => setDeveloper5(e.target.value)}
+       
+        />
     <button type='submit'>Save</button>
     </form>
   )
 }
 
 export default ProgramForm;
+
